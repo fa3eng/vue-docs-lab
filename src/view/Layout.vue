29ff5b6c4c50a307b945fb344c7e5e9135d0ade0
meakle <meakle@qq.com>
2022-04-10 19:07:58 +0800

    init: set up project

---
+++ src/view/Layout.vue
@@ -0,0 +1,15 @@
+<template>
+  <h1>{{ title }}</h1>
+</template>
+
+<script>
+import Vue from 'vue'
+
+export default Vue.extend({
+  data () {
+    return {
+      title: 'layout'
+    }
+  }
+})
+</script>
